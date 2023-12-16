06daabdae4e6201d38bc459e10f57c92bd5a411f
Marte <kanepimees@gmail.com>
2023-12-16 15:25:33 +0200

    Fourth commitgit rem

--- src/D231216/KenEx.java
+++ src/D231216/KenEx.java
@@ -5,20 +5,19 @@ import java.util.Scanner;
 public class KenEx {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         System.out.println("Enter number 1: ");
         int number1 = scanner.nextInt();
         System.out.println("Enter number 2: ");
         int number2 = scanner.nextInt();
 
-        if (number1 < 30 && number2 < 30) {
+        if (number1 < 30 && number2 < 30)
             System.out.println("input lower");
-        } else if (number1 > 30 && number2 > 30) {
+        else if (number1 > 30 && number2 > 30)
             System.out.println("input greater");
-        } else if (number1 == 30 && number2 == 30) {
+        else if (number1 == 30 && number2 == 30)
             System.out.println("equals");
-        } else {
-            System.out.println("<>");
-        }
+        else System.out.println("<>");
     }
 }
 /*
