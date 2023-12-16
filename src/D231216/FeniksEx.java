7cec2a6444ced7ab58c872f3e5fe19c688996269
Marte <kanepimees@gmail.com>
2023-12-16 15:33:21 +0200

    Fourth commit

--- src/D231216/FeniksEx.java
+++ src/D231216/FeniksEx.java
@@ -25,7 +25,9 @@ public class FeniksEx {
         }
     }
 }
-/*Write a simple "echo" application, that will:
+/*
+Write a simple "echo" application, that will:
 print back entered string,
 go to the beginning of a loop if user will enter "continue",
-break the loop with a "good bye!" message, if user will enter "quit".*/
+break the loop with a "goodbye!" message, if user will enter "quit".
+*/
