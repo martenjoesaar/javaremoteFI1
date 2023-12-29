1cb1d73ee785b5bb46fecce06c2fcfd5ce09c432
Marten Jõesaar <marten.joesaar@gmail.com>
2023-12-29 12:58:14 +0200

    Added printouts to JoosepEx for better understanding

--- src/D231216/JoosepEx.java
+++ src/D231216/JoosepEx.java
@@ -11,11 +11,14 @@ public class JoosepEx {
         int[] rearrangeArray = new int[length];
 
         for (int i = 0; i < length ; i++) {
+            System.out.println("Element " + i + ": " + intArray[i]);
             if (intArray[i] < 0) {
                 rearrangeArray[(length-1) - negcount] = intArray[i];
+                System.out.println(length + " - 1) - " + negcount + " = " + intArray[i]);
                 negcount++;
             } else {
                 rearrangeArray[poscount] = intArray[i];
+                System.out.println(poscount + " = " + intArray[i]);
                 poscount++;
 
             }
